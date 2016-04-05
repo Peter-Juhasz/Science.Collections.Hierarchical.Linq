c76c00c33834a83fc298607bcfcf5aa5a81e8f33
Peter-Juhasz <peter.juhasz@live.com>
2016-04-05 21:30:55 +0200

    Introducing forests.

--- TXMain/Build.cs
+++ TXMain/Build.cs
@@ -10,12 +10,13 @@ namespace System.Collections.Hierarchical
     public static partial class TreeNodeExtensions
     {
         /// <summary>
-        /// Builds a tree using the essential operations.
+        /// Realizes trees using essential operators.
         /// </summary>
         /// <param name="childrenSelector">Gets the child values of a given value.</param>
         /// <param name="parentSelector">Gets the parent value of a given value.</param>
         /// <param name="hasParent">Determines whether a given value has a parent value or not.</param>
-        public static IEnumerable<ITreeNode<T>> ToTree<T>(
+        /// <returns>Root nodes of the realized trees.</returns>
+        public static IEnumerable<ITreeNode<T>> ToForest<T>(
             this IEnumerable<T> source,
             Func<T, IEnumerable<T>> childrenSelector,
             Func<T, T> parentSelector,
@@ -35,15 +36,16 @@ namespace System.Collections.Hierarchical
                 throw new ArgumentNullException(nameof(hasParent));
 
 
-            return source.Select(e => new TreeNode<T>(e, childrenSelector, parentSelector, hasParent));
+            return source.Select(e => new TreeNode<T>(e, childrenSelector, parentSelector, hasParent)).Roots();
         }
 
         /// <summary>
-        /// Builds a tree using a <paramref name="childrenSelector"/> and <paramref name="parentSelector"/>. Null value for parent value indicates a root node.
+        /// Builds trees using a <paramref name="childrenSelector"/> and <paramref name="parentSelector"/>. Null value for parent value indicates a root node.
         /// </summary>
         /// <param name="childrenSelector">Gets the child values of a given value.</param>
         /// <param name="parentSelector">Gets the parent value of a given value.</param>
-        public static IEnumerable<ITreeNode<T>> ToTree<T>(
+        /// <returns>Root nodes of the realized trees.</returns>
+        public static IEnumerable<ITreeNode<T>> ToForest<T>(
             this IEnumerable<T> source,
             Func<T, IEnumerable<T>> childrenSelector,
             Func<T, T> parentSelector
@@ -60,7 +62,7 @@ namespace System.Collections.Hierarchical
                 throw new ArgumentNullException(nameof(parentSelector));
             
 
-            return source.ToTree(
+            return source.ToForest(
                 childrenSelector,
                 parentSelector,
                 hasParent: e2 => parentSelector(e2) != default(T)
@@ -68,37 +70,15 @@ namespace System.Collections.Hierarchical
         }
 
         /// <summary>
-        /// Realizes a tree based on only a children selector. Note: findig parent nodes have a high performance impact.
-        /// </summary>
-        /// <param name="childrenSelector">Selector for selecting child values.</param>
-        public static IEnumerable<ITreeNode<T>> ToTree<T>(
-            this IEnumerable<T> source,
-            Func<T, IEnumerable<T>> childrenSelector
-        )
-        {
-            if (source == null)
-                throw new ArgumentNullException(nameof(source));
-
-            if (childrenSelector == null)
-                throw new ArgumentNullException(nameof(childrenSelector));
-            
-
-            return source.ToTree(
-                childrenSelector,
-                parentSelector: e => source.SingleOrDefault(e2 => childrenSelector(e2).Contains(e)),
-                hasParent: e => source.Any(e2 => childrenSelector(e2).Contains(e))
-            );
-        }
-
-        /// <summary>
-        /// Realizes a tree based on a foreign key relationship.
+        /// Realizes trees based on a foreign key relationship.
         /// </summary>
         /// <typeparam name="T">Type of an entity.</typeparam>
         /// <typeparam name="TKey">Type of the key of an entity.</typeparam>
         /// <param name="keySelector">Selector for the primary key of an entity.</param>
         /// <param name="parentKeySelector">Selector for the foreign key of an entity.</param>
+        /// <returns>Root nodes of the realized trees.</returns>
         /// <example>categories.ToTree(c => c.Id, c => c.ParentId)</example>
-        public static IEnumerable<ITreeNode<T>> ToTree<T, TKey>(
+        public static IEnumerable<ITreeNode<T>> ToForest<T, TKey>(
             this IEnumerable<T> source,
             Func<T, TKey> keySelector,
             Func<T, TKey> parentKeySelector
@@ -115,16 +95,100 @@ namespace System.Collections.Hierarchical
                 throw new ArgumentNullException(nameof(parentKeySelector));
             
 
-            return source.Select(e =>
-                new TreeNode<T>(e,
-                    childrenSelector: c => source.Where(c2 => parentKeySelector(c2).Equals(keySelector(c))),
-                    parentSelector: c => source.Single(c2 => keySelector(c2).Equals(parentKeySelector(c))),
-                    hasParent: c => source.Any(c2 => keySelector(c2).Equals(parentKeySelector(c)))
-                )
+            return source.ToForest(
+                childrenSelector: c => source.Where(c2 => parentKeySelector(c2).Equals(keySelector(c))),
+                parentSelector: c => source.Single(c2 => keySelector(c2).Equals(parentKeySelector(c))),
+                hasParent: c => source.Any(c2 => keySelector(c2).Equals(parentKeySelector(c)))
             );
         }
 
-        
+        /// <summary>
+        /// Realizes trees based on only a children selector. Note: findig parent nodes can have a high performance impact.
+        /// </summary>
+        /// <param name="childrenSelector">Selector for selecting child values.</param>
+        /// <returns>Root nodes of the realized trees.</returns>
+        public static IEnumerable<ITreeNode<T>> ToForest<T>(
+            this IEnumerable<T> source,
+            Func<T, IEnumerable<T>> childrenSelector
+        )
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+
+            if (childrenSelector == null)
+                throw new ArgumentNullException(nameof(childrenSelector));
+
+
+            return source.ToForest(
+                childrenSelector,
+                parentSelector: e => source.SingleOrDefault(e2 => childrenSelector(e2).Contains(e)),
+                hasParent: e => source.Any(e2 => childrenSelector(e2).Contains(e))
+            );
+        }
+
+
+        /// <summary>
+        /// Builds a tree using the essential operations and returns the root node.
+        /// </summary>
+        /// <param name="childrenSelector">Gets the child values of a given value.</param>
+        /// <param name="parentSelector">Gets the parent value of a given value.</param>
+        /// <param name="hasParent">Determines whether a given value has a parent value or not.</param>
+        /// <returns>Root <see cref="ITreeNode{T}"/> of the tree.</returns>
+        public static ITreeNode<T> ToTree<T>(
+            this IEnumerable<T> source,
+            Func<T, IEnumerable<T>> childrenSelector,
+            Func<T, T> parentSelector,
+            Func<T, bool> hasParent
+        )
+        {
+            return source.ToForest(childrenSelector, parentSelector, hasParent).Single();
+        }
+
+        /// <summary>
+        /// Build a tree using a <paramref name="childrenSelector"/> and <paramref name="parentSelector"/>. <code>null</code> value for parent value indicates a root node.
+        /// </summary>
+        /// <param name="childrenSelector">Gets the child values of a given value.</param>
+        /// <param name="parentSelector">Gets the parent value of a given value.</param>
+        public static ITreeNode<T> ToTree<T>(
+            this IEnumerable<T> source,
+            Func<T, IEnumerable<T>> childrenSelector,
+            Func<T, T> parentSelector
+        )
+            where T : class
+        {
+            return source.ToForest(childrenSelector, parentSelector).Single();
+        }
+
+        /// <summary>
+        /// Builds a tree based on foreign key relationships.
+        /// </summary>
+        /// <typeparam name="T">Type of an entity.</typeparam>
+        /// <typeparam name="TKey">Type of the key of an entity.</typeparam>
+        /// <param name="keySelector">Selector for the primary key of an entity.</param>
+        /// <param name="parentKeySelector">Selector for the foreign key of an entity.</param>
+        /// <example>categories.ToTree(c => c.Id, c => c.ParentId)</example>
+        public static ITreeNode<T> ToTree<T, TKey>(
+            this IEnumerable<T> source,
+            Func<T, TKey> keySelector,
+            Func<T, TKey> parentKeySelector
+        )
+            where TKey : IEquatable<TKey>
+        {
+            return source.ToForest(keySelector, parentKeySelector).Single();
+        }
+
+        /// <summary>
+        /// Builds a tree based on only a children selector. Note: findig parent nodes can have a high performance impact.
+        /// </summary>
+        /// <param name="childrenSelector">Selector for selecting child values.</param>
+        public static ITreeNode<T> ToTree<T>(
+            this IEnumerable<T> source,
+            Func<T, IEnumerable<T>> childrenSelector
+        )
+        {
+            return source.ToForest(childrenSelector).Single();
+        }
+
 
         private sealed class TreeNode<T> : ITreeNode<T>
         {
--- TXMain/Operators.cs
+++ TXMain/Operators.cs
@@ -7,15 +7,22 @@ namespace System.Collections.Hierarchical
     public static partial class TreeNodeExtensions
     {
         /// <summary>
+        /// Gets the root <see cref="ITreeNode{T}"/>s of a set of <see cref="ITreeNode{T}"/>s.
+        /// </summary>
+        public static IEnumerable<ITreeNode<T>> Roots<T>(this IEnumerable<ITreeNode<T>> source)
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+
+
+            return source.Where(n => n.IsRoot());
+        }
+        /// <summary>
         /// Gets the single root <see cref="ITreeNode{T}"/> from a set of <see cref="ITreeNode{T}"/>s.
         /// </summary>
         public static ITreeNode<T> Root<T>(this IEnumerable<ITreeNode<T>> source)
         {
-            if (source == null)
-                throw new ArgumentNullException(nameof(source));
-            
-
-            return source.Single(n => n.IsRoot());
+            return source.Roots().Single();
         }
         
 
