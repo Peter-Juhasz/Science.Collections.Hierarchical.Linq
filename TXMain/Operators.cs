6dac2250a8f2584497fc4abf3ef3fe84914b59b8
Peter-Juhasz <peter.juhasz@live.com>
2016-04-05 23:47:32 +0200

    Merged siblings and 'siblings and self'.

--- TXMain/Operators.cs
+++ TXMain/Operators.cs
@@ -139,36 +139,33 @@ namespace System.Collections.Hierarchical
 
         }
 
-
-        /// <summary>
-        /// Gets the sibling <see cref="ITreeNode{T}"/>s of a given <see cref="ITreeNode{T}"/>.
-        /// </summary>
-        public static IEnumerable<ITreeNode<T>> Siblings<T>(
-            this ITreeNode<T> node,
-            LevelTraverseDirection direction = LevelTraverseDirection.LeftToRight
-        )
-        {
-            return node.SiblingsAndSelf(direction).Except(new[] { node });
-        }
-
+        
         /// <summary>
         /// Gets the sibling <see cref="ITreeNode{T}"/>s of a given <see cref="ITreeNode{T}"/> including self.
         /// </summary>
-        public static IEnumerable<ITreeNode<T>> SiblingsAndSelf<T>(
+        public static IEnumerable<ITreeNode<T>> Siblings<T>(
             this ITreeNode<T> node,
-            LevelTraverseDirection direction = LevelTraverseDirection.LeftToRight
+            LevelTraverseDirection direction = LevelTraverseDirection.LeftToRight,
+            bool includeSelf = false
         )
         {
             if (node == null)
                 throw new ArgumentNullException(nameof(node));
             
 
+            // root node has no siblings
             if (node.IsRoot())
                 return Enumerable.Empty<ITreeNode<T>>();
 
-            return node.Parent().Children()
-                .ReverseIf(direction == LevelTraverseDirection.RightToLeft)
-            ;
+            // siblings and self
+            IEnumerable<ITreeNode<T>> result = node.Parent().Children()
+                .ReverseIf(direction == LevelTraverseDirection.RightToLeft);
+
+            // exclude self if not needed
+            if (!includeSelf)
+                result = result.Except(new[] { node });
+
+            return result;
         }
 
 
